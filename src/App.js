9312a466bae6769a786252495504fe43254c7fb1
saldocc <onurcoban1109@gmail.com>
2021-02-09 15:27:02 +0300

    shop item and preview created

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import { Route } from "react-router-dom";
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
-
-const MansPage = () => (<div>
-  <h1>Mans</h1>
-</div>)
+import ShopPage from './pages/shop/shop.component'
 
 
 
@@ -14,7 +11,7 @@ function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage}></Route>
-      <Route path="/mans" component={MansPage}></Route>
+      <Route path="/shop" component={ShopPage}></Route>
     </div>
   );
 }
---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './collection-item.styles.scss'
+
+const CollectionItem = ({ id, name, price, imageUrl }) => {
+	return (
+		<div className="collection-item">
+			<img src={imageUrl} className="image" />
+			<div className="collection-footer">
+				<div className="collection-footer-info">
+					<span className="name">{name}</span>
+					<span className="price">{price}</span>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default CollectionItem
